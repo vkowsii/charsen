3e11f9bfa353dfa00b23470ad5b50ca2b79a6a40
vkowsii <30829409+vkowsii@users.noreply.github.com>
2018-03-07 16:38:03 +0530

    Create charsen.c

---
+++ charsen.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    str s[100];
+    int count=0,i;
+    printf("enter the charecter");
+    scanf("%s",s);
+    for(i=0;s[i]1='0';i++)
+    {
+        if(s[i]=='')
+        count++;
+    }
+    printf("%d",count++i);
+}
+    }
